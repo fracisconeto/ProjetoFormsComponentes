76b6133af030abef0cfcb3fe89fc23ac792a3fe8
fracisconeto <vargasfrancisco227@gmail.com>
2024-07-22 16:45:57 -0300

    first commit

---
+++ src/components/Forms.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+
+<template>
+<h1>vargas lindo</h1>
+</template>
+
+
+<style scoped>
+
+h1{
+color:saddlebrown ;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import Forms from './components/Forms.vue'
+
+createApp(App).mount('#app')
